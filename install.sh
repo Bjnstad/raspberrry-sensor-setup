157ecbbbcfc09e8eacba339d2aa4f77fade7369a
Axel Bjoernstad <axebjo@gmail.com>
2019-04-21 01:10:22 +0100

    bugfixes

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@
 
 # Update packages
 apt-get update
-apt-get upgrade
+apt-get -y upgrade
 
 # Install requirements for accesspoint dnsmasq and hostapd
-apt-get install dnsmasq hostapd
+apt-get -y install dnsmasq hostapd
 
 # Stop dnsmasq and hostapd until fully configured
 systemctl stop dnsmasq
@@ -20,7 +20,7 @@ systemctl stop hostapd
 
 # Configure a static interface on wlan0
 # TODO: Maybye setup via command
-ip addr add 192.168.0.172/24 dev wlan0
+ip addr add 192.168.4.1/24 dev wlan0
 
 # Taking backup of dnsmasq.conf, then setting static ip for wlan0 
 mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
@@ -31,17 +31,17 @@ dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h
 
 # Move config file for access point
 # TODO: Make access point public open
-mv ./configs/hostapd.conf /etc/hostapd/hostapd.conf
+cp ./configs/hostapd.conf /etc/hostapd/hostapd.conf
 
 # Apply the new config
-# TODO: todo
+sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/g' /etc/sysctl.conf
 
 # Start hostapd and dnsmasqi back on
 systemctl start hostapd
 systemctl start dnsmasqi
 
 # Add routing and masquerade
-sed -i 's/net.ipv4.ip_forward=1/#net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+sed -i 's/net.ipv4.ip_forward\=1/#net.ipv4.ip_forward\=1/g' /etc/sysctl.conf
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sh -c "iptables-save > /etc/iptables.ipv4.nat"
 sed -i 's/exit 0/iptables-restore < /etc/iptables.ipv4.nat \n exit 0/g' /etc/sysctl.conf
