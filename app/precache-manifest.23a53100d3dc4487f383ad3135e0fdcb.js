4ace490fa7e23ee73c35f4bfe58756dd47e7a68a
Axel Bjørnstad <axebjo@gmail.com>
2019-05-20 18:44:26 +0200

    compiled version of connect app

---
+++ app/precache-manifest.23a53100d3dc4487f383ad3135e0fdcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdee62dbb610f5291d240152cfec2cda",
+    "url": "192.168.4.1/index.html"
+  },
+  {
+    "revision": "9d18afb9b34b63602eb8",
+    "url": "192.168.4.1/static/css/main.b3628cab.chunk.css"
+  },
+  {
+    "revision": "ce323d7c00cb98fdf51d",
+    "url": "192.168.4.1/static/js/2.2f379bf9.chunk.js"
+  },
+  {
+    "revision": "9d18afb9b34b63602eb8",
+    "url": "192.168.4.1/static/js/main.0e17a66b.chunk.js"
+  },
+  {
+    "revision": "fe88027f7a57f8625fc6",
+    "url": "192.168.4.1/static/js/runtime~main.f6d0b402.js"
+  }
+]);
